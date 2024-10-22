5a7e51eaf52e7342592230a56c33a4685857d73f
Passenger1998 <98896021+Passenger1998@users.noreply.github.com>
2024-10-22 21:47:15 +0100

    feature
    player control movement

--- Assets/Tests/Mechanics/001/T_M_001_001_All.cs
+++ Assets/Tests/Mechanics/001/T_M_001_001_All.cs
@@ -19,7 +19,7 @@ public class T_M_001_001_All : MonoBehaviour
     [Serializable]
     public enum Direction
     {
-        front, back, left, right,none
+        front, back, left, right, none
     }
 
     [Serializable]
@@ -38,20 +38,34 @@ public class T_M_001_001_All : MonoBehaviour
     }
 
     public struct B_PlayerControl_Result
-    { }
+    {
+        public Direction direciton;
+        public bool isMoving;
+    }
 
-    public void B_PlayerControl(B_PlayerControl_Data data)
+    public B_PlayerControl_Result B_PlayerControl(B_PlayerControl_Data data)
     {
         InputMethod inputMethod_local = data.inputMethod;
+        bool isMoving_local = data.isMoving;
+        float speed_local = data.speed;
+        GameObject player_local = data.player;
+
+        B_PlayerControl_Result result = new B_PlayerControl_Result();
 
         F_InputMove_Data data_result_F_InputMove = new F_InputMove_Data
         {
             inputMethod = inputMethod_local,
+            isMoving = isMoving_local,
+            speed = speed_local,
+            player= player_local
 
-        };
+};
         F_InputMove_Result result_F_InputMove = F_InputMove(data_result_F_InputMove);
 
+        result.direciton = result_F_InputMove.direciton;
+        result.isMoving = result_F_InputMove.isMoving;
 
+        return result;
     }
 
     public struct F_InputMove_Data
@@ -83,57 +97,62 @@ public class T_M_001_001_All : MonoBehaviour
         {
             case InputMethod.developerdebug:
                 {
-                    #region input enter
-                    if (Input.GetKeyDown(KeyCode.A))
+                    //#region input enter
+                    //if (Input.GetKeyDown(KeyCode.W))
+                    //{
+                    //    direction_local = Direction.front;
+                    //    isMoving_local = true;
+                    //}
+                    //else if (Input.GetKeyDown(KeyCode.S))
+                    //{
+                    //    direction_local = Direction.back;
+                    //    isMoving_local = true;
+                    //}
+                    //else if (Input.GetKeyDown(KeyCode.A))
+                    //{
+                    //    direction_local = Direction.left;
+                    //    isMoving_local = true;
+                    //}
+                    //else if (Input.GetKeyDown(KeyCode.D))
+                    //{
+                    //    direction_local = Direction.right;
+                    //    isMoving_local = true;
+                    //}
+                    //else
+                    //{
+                    //    direction_local = Direction.none;
+                    //    isMoving_local = false;
+                    //}
+                    //#endregion
+                    #region input hold
+                    if (Input.GetKey(KeyCode.W))
                     {
                         direction_local = Direction.front;
                         isMoving_local = true;
+                        player_local.transform.position += new Vector3(0, 0, speed_local) * Time.deltaTime;
                     }
-                    else if (Input.GetKeyDown(KeyCode.W))
+                    else if (Input.GetKey(KeyCode.S))
                     {
                         direction_local = Direction.back;
                         isMoving_local = true;
+                        player_local.transform.position += new Vector3(0, 0, -speed_local) * Time.deltaTime;
                     }
-                    else if (Input.GetKeyDown(KeyCode.A))
+                    else if (Input.GetKey(KeyCode.A))
                     {
                         direction_local = Direction.left;
                         isMoving_local = true;
+                        player_local.transform.position += new Vector3(-speed_local, 0, 0) * Time.deltaTime;
                     }
-                    else if (Input.GetKeyDown(KeyCode.D))
+                    else if (Input.GetKey(KeyCode.D))
                     {
                         direction_local = Direction.right;
                         isMoving_local = true;
-                    }
-                    else
-                    {
-                        direction_local = Direction.none;
-                        isMoving_local = false;
-                    }
-                    #endregion
-                    #region input hold
-                    if (Input.GetKey(KeyCode.A)
-                        && direction_local == Direction.front)
-                    {
-                        player_local.transform.position += new Vector3(0, 0, speed_local) * Time.deltaTime;
-                    }
-                    else if (Input.GetKey(KeyCode.W)
-                        && direction_local == Direction.back)
-                    {
-                        player_local.transform.position += new Vector3(0, 0, -speed_local) * Time.deltaTime;
-                    }
-                    else if (Input.GetKey(KeyCode.A)
-                        && direction_local == Direction.left)
-                    {
-                        player_local.transform.position += new Vector3(-speed_local, 0, 0) * Time.deltaTime;
-                    }
-                    else if (Input.GetKey(KeyCode.D)
-                        && direction_local == Direction.right)
-                    {
                         player_local.transform.position += new Vector3(speed_local, 0, 0) * Time.deltaTime;
                     }
                     else
                     {
                         direction_local = Direction.none;
+                        isMoving_local = false;
                     }
                     #endregion
                 };
@@ -157,13 +176,27 @@ public class T_M_001_001_All : MonoBehaviour
 
     public void F_SlowRotation(F_SlowRotation_Data data)
     {
-        
+
     }
     #endregion
 
     private void Update()
     {
+        B_PlayerControl_Data B_PlayerControl_data = new B_PlayerControl_Data
+        {
+            inputMethod = inputMethod,
+            isMoving = isMoving,
+            speed = speed,
+            player = player
+        };
 
-        B_PlayerControl(inputMethod);
+        B_PlayerControl_Result result_B_PlayerControl = B_PlayerControl(B_PlayerControl_data);
+
+        direction_current = result_B_PlayerControl.direciton;
+        isMoving = result_B_PlayerControl.isMoving;
+
+        Debug.Log(direction_current);
+
+
     }
 }
