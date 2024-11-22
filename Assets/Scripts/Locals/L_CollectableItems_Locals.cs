f3fb7159506d553e759fce84758551407177d67d
Passenger1998 <98896021+Passenger1998@users.noreply.github.com>
2024-11-22 15:30:00 +0000

    add
    - new room layout (working)

--- Assets/Scripts/Locals/L_CollectableItems_Locals.cs
+++ Assets/Scripts/Locals/L_CollectableItems_Locals.cs
@@ -46,7 +46,7 @@ public class L_CollectableItems_Locals : MonoBehaviour
 
         if (isCollectable && player_trans != null)
         {
-            rb.AddForce((player_trans.position - this.transform.position)*5);
+            rb.AddForce((player_trans.position - this.transform.position)*10);
             if (this.gameObject.transform.localScale.x >= 0.01)
             {
                 this.gameObject.transform.localScale -= Vector3.one * 0.5f * Time.deltaTime;
